f70ca2398240d934765e7b94a6c4c88767906d51
zhangkai10@cfldcn.com <zk694269416>
2018-11-09 13:05:03 +0800

    fixbug

--- text.py
+++ text.py
@@ -115,10 +115,11 @@ class GetTextList:
 
     def getHtml(self):
         chromeOptions = webdriver.ChromeOptions()
-        chromeOptions.add_argument('--headless')
+        chromeOptions.add_argument("--headless")
         chromeOptions.add_argument('--no-sandbox')
         chromeOptions.add_argument('--disable-gpu')
         chromeOptions.add_argument('--disable-dev-shm-usage')
+        chromeOptions.add_argument('--window-size=1024,768')
         self.brower = webdriver.Chrome(chrome_options=chromeOptions)
         print("--------", "开始获取图文列表信息", "--------")
         self.getData(self.baseUrl)
