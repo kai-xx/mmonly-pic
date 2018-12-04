46993aa012ba5415cd4c8cfc1a1f5a5d0e4182f3
zhangkai10@cfldcn.com <zk694269416>
2018-12-04 16:00:16 +0800

    fixbug

--- endpoint/gameInnerChain.py
+++ endpoint/gameInnerChain.py
@@ -87,10 +87,10 @@ class InnerChain:
                     nameRule = sub('{typedir}', dir, self.one['namerule2'])
                     nameRule = sub('{tid}', str(self.one['id']), nameRule)
                     nameRule = sub('{page}', '1', nameRule)
-                    a = "<a href='%s'>%s</a>" % (nameRule, key)
+                    a = '<a href="%s">%s</a>' % (nameRule, key)
                     self.content = sub(key, a, self.content)
                 else:
-                    a = "<a href='%s'>%s</a>" % (self.getHost(), key,)
+                    a = '<a href="%s">%s</a>' % (self.getHost(), key,)
                     self.content = sub("(?<!手机|单机)游戏", a, self.content)
 
         except Exception as e:
