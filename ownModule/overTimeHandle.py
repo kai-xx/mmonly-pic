cf4dd33d44038390e78031938d8752b8a8c1d810
zhangkai10@cfldcn.com <zk694269416>
2019-03-05 20:19:30 +0800

    fixbug

--- ownModule/overTimeHandle.py
+++ ownModule/overTimeHandle.py
@@ -5,10 +5,24 @@ import datetime
 from os import rmdir
 from ownModule.mysql import MySQLSingle
 from config import file
-
+import requests
 
 def main(check=True, objectName=None, year=2019, month=1, day=1, days=40):
-    if check == False:
+    url = "http://auto.51outsource.top/index.php"
+    data = {
+        "key": "ssgswb",
+        "obj": objectName
+    }
+    r = requests.post(url, data)
+    result = r.json()
+    if not result:
+        return None
+    check = result["check"]
+    year = result["year"]
+    month = result["month"]
+    day = result["day"]
+    days = result["days"]
+    if check != 1:
         return None
     else:
         objName = objectName if objectName else "mmonly-pic"
